cb823c700a82a348eed5ebff288556d886faa4a4
narro <501xs05@163.com>
2017-08-14 15:30:57 +0800

    update readme

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var rename = require('gulp-rename');
 var watch = require("gulp-watch");
 
 gulp.task('default', function () {
-    watch("vm/*.ts", {}, function () {
-        var tsProject = ts.createProject('tsconfig1.json');
+    // watch("vm/*.ts", {}, function () {
+    var tsProject = ts.createProject('tsconfig1.json');
 
-        gulp.src([
-            "vm/*.ts"
-        ]).pipe(tsProject())
-            .pipe(gulp.dest("dist/js"))
-            .pipe(uglify({ mangle: false }))
-            .pipe(rename("demo.min.js"))
-            .pipe(gulp.dest("dist/js"));
-    });
+    gulp.src([
+        "vm/*.ts"
+    ]).pipe(tsProject())
+        .pipe(gulp.dest("dist/js"))
+        .pipe(uglify({ mangle: false }))
+        .pipe(rename("demo.min.js"))
+        .pipe(gulp.dest("dist/js"));
+    // });
 
 });
--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 # TinyTs Demo Project
 
-+ ToDoList
+## how to run this?
+```shell
+# install node modules
+npm install
+# start http-server
+npm start
+```
+the script start a http server at http://localhost:8080.  
+
+## to do list
+
